242416978ba5bfafafd0aafb3224a657ce900b58
Brae Baumgartner <brae.baumgartner@gmail.com>
2017-11-30 21:26:01 -0500

    Initial commit

---
+++ src/GameOfLife/GameOfLifeTest.java
@@ -0,0 +1,14 @@
+package GameOfLife;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameOfLifeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
